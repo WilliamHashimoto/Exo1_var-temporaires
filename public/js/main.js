ca63846c005c0250f90ae2d16ac05b77096649a4
WilliamHashimoto <79861381+WilliamHashimoto@users.noreply.github.com>
2021-04-19 17:06:11 +0200

    fini

---
+++ public/js/main.js
@@ -0,0 +1,74 @@
+// let a = 1; 
+// let b = 2; 
+// let c = 3;
+
+// let temp;
+
+// temp = a;
+// a = b;
+// b = c;
+// c = temp;
+
+// console.log(a, b, c);
+
+//Exo1
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+
+// temp = a
+// a = c;
+// c = b;
+// b = a;
+// a = d;
+// d = temp;
+
+
+// console.log(a, b, c, d);
+
+//Exo2
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+
+// let temp = a;
+
+// a = b;
+// b = c;
+// c = d;
+// d = temp;
+
+// console.log(a,b,c,d);
+
+//Exo3
+// let tableau = [1, 2, 3, 4];
+
+// let temp = tableau[1]
+
+// tableau[1] = tableau[2];
+// tableau[2] = temp;
+// temp = tableau[3];
+// tableau[3] = tableau[0];
+// tableau[0] = temp;
+
+//Exo 4
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+let f = 6;
+
+let temp = c;
+
+c = a; 
+a = d;
+d = b;
+b = e;
+e = f;
+f = temp;
+
+console.log(a,b,c,d,e,f);
+
